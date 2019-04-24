c33bc9188686a8c4a895f8025a14d0380aa39b61
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-25 01:41:37 +0530

    Added api to all routes

--- app/routes/um/department.routes.js
+++ app/routes/um/department.routes.js
@@ -2,17 +2,17 @@ module.exports = (app) => {
     const departments = require('../../controllers/um/department.controller.js');
 
     // Create a new Note
-    app.post('/departments', departments.create);
+    app.post('/api/departments', departments.create);
 
     // Retrieve all Notes
-    app.get('/departments', departments.findAll);
+    app.get('/api/departments', departments.findAll);
 
     // Retrieve a single Note with noteId
-    app.get('/departments/:id', departments.findOne);
+    app.get('/api/departments/:id', departments.findOne);
 
     // Update a Note with noteId
-    app.put('/departments/:id', departments.update);
+    app.put('/api/departments/:id', departments.update);
 
     // Delete a Note with noteId
-    app.delete('/departments/:id', departments.delete);
+    app.delete('/api/departments/:id', departments.delete);
 }
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,17 +2,17 @@ module.exports = (app) => {
     const users = require('../controllers/user.controller.js');
 
     // Create a new Note
-    app.post('/users', users.create);
+    app.post('/api/users', users.create);
 
     // Retrieve all Notes
-    app.get('/users', users.findAll);
+    app.get('/api/users', users.findAll);
 
     // Retrieve a single Note with noteId
-    app.get('/users/:id', users.findOne);
+    app.get('/api/users/:id', users.findOne);
 
     // Update a Note with noteId
-    app.put('/users/:id', users.update);
+    app.put('/api/users/:id', users.update);
 
     // Delete a Note with noteId
-    app.delete('/users/:id', users.delete);
+    app.delete('/api/users/:id', users.delete);
 }
