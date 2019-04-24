73981de982a1ea5c5b9715374d2407b0416e5864
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-25 00:55:15 +0530

    Added headers

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 // create express app
 const app = express();
-const cors = require("cors");
-app.use(cors());
+
+
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 // Configuring the database
 const dbConfig = require('./config/database.config.js');
@@ -22,7 +24,7 @@ mongoose.Promise = global.Promise;
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
-    console.log("Successfully connected to the database");    
+    console.log("Successfully connected to the database");
 }).catch(err => {
     console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
@@ -30,11 +32,15 @@ mongoose.connect(dbConfig.url, {
 
 // define a simple route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.json({ "message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes." });
+    next();
 });
+
 require('./app/routes/user.routes.js')(app);
-
-
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
