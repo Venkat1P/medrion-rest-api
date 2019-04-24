db75fa1e30c2b7236c1210b3d05cb7508546a4a3
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-24 07:48:08 +0530

    Removed unwanted code in server.js

--- server.js
+++ server.js
@@ -31,10 +31,6 @@ app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
 require('./app/routes/user.routes.js')(app);
-// listen for requests
-// app.listen(3000, () => {
-//     console.log("Server is listening on port 3000");
-// });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
