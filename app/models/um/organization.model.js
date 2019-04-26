9d4de77065e3bf2e3e6b32610dcd24895ccefc39
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-27 02:59:25 +0530

    Changes in model of org

--- app/models/um/organization.model.js
+++ app/models/um/organization.model.js
@@ -12,21 +12,20 @@ const orgSchema = mongoose.Schema(
         org_eap: Boolean,
         org_grants: Boolean,
         org_valid_up_to: String,
-        org_address_details: String,
+        org_address_1: String,
 
-        org_address_1: String,
-        org_address_1: String,
+        org_address_2: String,
         org_zip_code: String,
         org_city: String,
         org_country: String,
-
         org_state: String,
+
         org_phone_no: String,
         org_fax_no: String,
         org_website: String,
         org_emailId: String,
-
         org_title: String,
+
         org_main_header: String,
         org_sub_header: String,
         
