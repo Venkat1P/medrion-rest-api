90add156fff5154587e90aeea7b52a7aff7aadbf
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-27 10:59:48 +0530

    removed un wanted code

--- server.js
+++ server.js
@@ -48,10 +48,6 @@ var sessionChecker = (req, res, next) => {
 };
 
 app.get('/api', sessionChecker, (req, res) => {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     res.redirect('/api/login');
 });
 
@@ -68,10 +64,6 @@ app.route('/api/login')
             } else if (password !== user.password) {
                 res.json({ "isLoggedIn": false, "message": "Password is wrong" });
             } else {
-                res.header('Access-Control-Allow-Credentials', true);
-                res.header("Access-Control-Allow-Origin", "*");
-                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-                res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
                 req.session.user = user;
                 res.json({ isLoggedIn: true, userDetails: user });
             }
