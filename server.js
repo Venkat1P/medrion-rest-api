b313ddd3209e351285d11c94faddc1842e4da235
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-27 12:43:36 +0530

    Made wild card

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
     res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
