a20a63f68bbc4867468e292958f545270452c01c
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-27 13:11:08 +0530

    changes ds s

--- server.js
+++ server.js
@@ -27,7 +27,22 @@ mongoose.connect(dbConfig.url, {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+//app.use(cors());
+app.use(function(req, res, next) {
+
+    //to allow cross domain requests to send cookie information.
+    res.header('Access-Control-Allow-Credentials', true);
+    
+    // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
+    res.header('Access-Control-Allow-Origin',  req.headers.origin);
+    
+    // list of methods that are supported by the server
+    res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,DELETE');
+    
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    
+    next();
+    });
 
 app.use(session({
     key: 'user_sid',
