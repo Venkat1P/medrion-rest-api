3804f5de0ff9d67450c851a23563f6dc9a3e0831
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-27 13:00:14 +0530

    changes ds s

--- server.js
+++ server.js
@@ -28,13 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
-    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
-    next();
-});
+
 
 app.use(session({
     key: 'user_sid',
