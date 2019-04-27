7ed37b1e6f72e78605f8f5585b8a3c77bebfc7a1
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-27 12:53:15 +0530

    Cors

--- server.js
+++ server.js
@@ -29,21 +29,21 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
-app.use(function(req, res, next) {
+// app.use(function(req, res, next) {
 
-    //to allow cross domain requests to send cookie information.
-    res.header('Access-Control-Allow-Credentials', true);
+//     //to allow cross domain requests to send cookie information.
+//     res.header('Access-Control-Allow-Credentials', true);
     
-    // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
-    res.header('Access-Control-Allow-Origin',  req.headers.origin);
+//     // origin can not be '*' when crendentials are enabled. so need to set it to the request origin
+//     res.header('Access-Control-Allow-Origin',  req.headers.origin);
     
-    // list of methods that are supported by the server
-    res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,DELETE');
+//     // list of methods that are supported by the server
+//     res.header('Access-Control-Allow-Methods','OPTIONS,GET,PUT,POST,DELETE');
     
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     
-    next();
-    });
+//     next();
+//     });
 
 app.use(session({
     key: 'user_sid',
