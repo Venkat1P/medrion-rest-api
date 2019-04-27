1880d79d806a262fdac38cd203d08dae5742ccf7
venkatsrikanth <venkat.ieee55@gmail.com>
2019-04-27 12:20:04 +0530

    Cross domain cookies issue

--- server.js
+++ server.js
@@ -28,20 +28,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(express.cookieSession({
-    key: 'myappname.sid',
-    secret: 'my secret',
-    cookie: { domain: '.medrion-daf7f.firebaseapp.com', maxAge: 600000 }
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+    next();
+});
+
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
 }));
-// app.use(session({
-//     key: 'user_sid',
-//     secret: 'somerandonstuffs',
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         expires: 600000
-//     }
-// }));
 
 var sessionChecker = (req, res, next) => {
     if (req.session.user && req.cookies.user_sid) {
